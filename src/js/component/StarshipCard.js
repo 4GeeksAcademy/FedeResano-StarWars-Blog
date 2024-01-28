630685847f7cfa1b0fad93ea1ff4a380be8b7924
Federico Resano <123256603+fede1525@users.noreply.github.com>
2024-01-28 20:02:46 +0000

    commit

---
+++ src/js/component/StarshipCard.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+
+const StarshipCard = () => {
+    const { store, actions } = useContext(Context);
+
+    return (
+        <div className="d-flex">
+            {store.starships.map((element) => {
+                return (
+                    <div className="container">
+                        <div className="card" key={element.uid}>
+                            <img src={`https://starwars-visualguide.com/assets/img/vehicles/${element.uid}.jpg`} className="card-img-top" />
+                            <div className="card-body">
+                                <h5 className="card-title">{element.properties.name}</h5>
+                                <p>Manufacturer: {element.properties.manufacturer}</p>
+                                <p>Model: {element.properties.model}</p>
+                                <p>Class: {element.properties.starship_class}</p>
+                            </div>
+
+                            <div className="d-flex justify-content-center">
+                                <Link to={`/vehicles/${element.uid}`}> Learn more</Link>
+                                <button className="btn btn-warning" onClick={() => actions.addFavorite(`/starships/${element.uid}`, element.properties.name)}>Add to favorites</button>
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
