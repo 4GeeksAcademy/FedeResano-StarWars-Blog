d9a82b91c861850b6282fde51591267cd1a16282
Federico Resano <123256603+fede1525@users.noreply.github.com>
2024-01-30 21:06:33 +0000

    commit

--- src/js/layout.js
+++ src/js/layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home, Browse, Character, Film, Planet, Species, Starship, Vehicle } from "./views/home";
+import { Home, Browse, Character, Film, Planet, Species, Starship, Vehicle, CharacterList, FilmsList, PlanetsList, SpeciesList, VehiclesList, StarshipsList } from "./views/home";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
